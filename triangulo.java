b8dd6ed874f968449dd9ba454a9edd18d01d6274
JoaoTeixeira01 <62699238+JoaoTeixeira01@users.noreply.github.com>
2020-04-01 19:02:18 +0100

    Add files via upload

---
+++ triangulo.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class triangulo {
+public static void main(String[] args) {
+	String crescente = "";
+	int valor = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+	for(int i = 1; i <= valor; i++) {
+		crescente += "*";
+		System.out.println(crescente);
+	}
+	}
+}
